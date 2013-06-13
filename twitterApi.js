c6a2ec341e8165c9149b56c58761d02e9eab1215
SlightlyCyborg <slightlycyborg@gmail.com>
2013-06-13 11:10:02 -0400

    routes refactor

---
+++ twitterApi.js
@@ -0,0 +1,25 @@
+    twit = require('twit');
+
+var twitt = new twit({
+    consumer_key: 'ObEAEbrbltL7CiWDiJMw',
+    consumer_secret: 'X7qIX7gMFGfCHDxrxCkpDuziZYz7u3otLd6vXVxHGo',
+    access_token: '426783140-64zTCFPKuGaC8BZ1f1QpxmWWgPhskiDqLOjHBVZQ',
+    access_token_secret: 'J14ouvmNaTfvP0pMaXXlf5JuYBBW5dVJ26pGwPg'
+});
+
+
+function filterStream(hashTag){
+   var stream = twitt.stream('statuses/filter', {track: hashTag}) ;
+
+   stream.on('tweet', function(tweet){
+      console.log(tweet.text); 
+   });
+}
+
+exports.filterStream = filterStream;
+
+
+
+
+
+
